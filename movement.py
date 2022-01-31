5fc7094fdd82b46a98255af291428215ea305c87
Adamfk1 <45567433+Adamfk1@users.noreply.github.com>
2022-01-31 19:08:30 +0100

    added classes, renamed diagram
    Added Vector and Movement

---
+++ movement.py
@@ -0,0 +1,10 @@
+class MovementData():
+    def __init__(self, vx, vy):
+        self.vx = vx
+        self.vy = vy
+        
+    def get_vx(self):
+        return self.vx
+    
+    def get_vy(self):
+        return self.vy
