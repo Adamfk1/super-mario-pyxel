36937c42ce976cb5d09e36141a64ebaf6c437cb7
Adamfk1 <45567433+Adamfk1@users.noreply.github.com>
2022-02-05 14:59:57 +0100

    changes

--- vector.py
+++ vector.py
@@ -3,9 +3,19 @@ class Vector():
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        
+
+    @property
     def get_x(self):
         return self.x
     
+    @property
     def get_y(self):
         return self.y
+    
+    @property
+    def get_vx(self):
+        return self.vx
+    
+    @property
+    def get_vy(self):
+        return self.vy
